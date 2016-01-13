a04ef57c128df05da37aea96a6369180a79cc865
Jack3113 <jack03113@gmail.com>
2016-01-13 17:21:01 +0100

    First Hello World

--- server/app.js
+++ server/app.js
@@ -4,20 +4,25 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({
-	extended: true
+    extended: true
 }));
 app.use(bodyParser.json());
 app.use(function (req, res, next) {
-	res.header('Access-Control-Allow-Origin', '*');
-	res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
-	res.header('Access-Control-Allow-Headers', 'Content-Type');
-	next();
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
 });
-
+app.use(express.static("client"));
+app.use("/bower_components", express.static("bower_components"));
+app.use("/js", express.static("js"));
+app.use("/design", express.static("design"));
+app.use("/assets", express.static("assets"));
+app.use("/partials", express.static("partials"));
 
 // Start server
 var server = app.listen(3001, function () {
-	var host = server.address().address;
-	var port = server.address().port;
-	console.log('Server listening at http://%s:%s', host, port);
+    var host = server.address().address;
+    var port = server.address().port;
+    console.log('Server listening at http://%s:%s', host, port);
 });
