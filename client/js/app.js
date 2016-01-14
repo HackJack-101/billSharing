9aacbdf0967f6be8256f96ee10344fe9696408b3
SylEmo <sylvain.emonet@orange.fr>
2016-01-14 23:48:30 +0100

    Complete index.html with topbar + global css and js

--- client/js/app.js
+++ client/js/app.js
@@ -5,5 +5,11 @@ var app = angular.module('app', ['ngRoute', 'ngMaterial']);
 app.config(['$routeProvider',
     function ($routeProvider) {
         $routeProvider.when('/', {templateUrl: 'partials/home.html'});
+        $routeProvider.when('/login', {templateUrl: 'partials/login.html'});
+        $routeProvider.when('/register', {templateUrl: 'partials/register.html'});
+        $routeProvider.when('/addgroup', {templateUrl: 'partials/addgroup.html'});
+        $routeProvider.otherwise({
+            redirectTo: "/"
+        });
     }
 ]);
