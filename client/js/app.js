e4e74efe0c8e2ca7a5200a0a3f8af6c121a9829e
SylEmo <sylvain.emonet@orange.fr>
2016-01-18 14:08:10 +0100

    Implementation userbills.html + add shortcut left panel + change functions for shortcuts

--- client/js/app.js
+++ client/js/app.js
@@ -5,7 +5,8 @@ var app = angular.module('app', ['ngRoute', 'ngMaterial']);
 app.config(['$routeProvider',
     function ($routeProvider) {
         $routeProvider.when('/', {templateUrl: 'partials/home.html'});
-        $routeProvider.when('/', {templateUrl: 'partials/recentactivity.html'});
+        $routeProvider.when('/recentactivity', {templateUrl: 'partials/recentactivity.html'});
+        $routeProvider.when('/allbills', {templateUrl: 'partials/userbills.html'});
         $routeProvider.when('/login', {templateUrl: 'partials/login.html'});
         $routeProvider.when('/register', {templateUrl: 'partials/register.html'});
         $routeProvider.when('/addgroup', {templateUrl: 'partials/addgroup.html'});
