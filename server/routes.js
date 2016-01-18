fa7c5ec28a5ffaf93c838e9a8d8bcdc3ac37d678
Jack3113 <jack03113@gmail.com>
2016-01-18 22:35:44 +0100

    Add payment route

--- server/routes.js
+++ server/routes.js
@@ -5,4 +5,5 @@ module.exports = function (app) {
 	app.use('/user', require('./api/user'));
 	app.use('/bill', require('./api/bill'));
 	app.use('/group', require('./api/group'));
+	app.use('/payment', require('./api/payment'));
 };
