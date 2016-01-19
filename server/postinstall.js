2811100e82c9eac39cb10f94142c446bb2ae0ff5
Jack3113 <jack03113@gmail.com>
2016-01-19 17:26:32 +0100

    End postinstall script

--- server/postinstall.js
+++ server/postinstall.js
@@ -2,9 +2,9 @@
 
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/Jack3113_billSharing', function (err) {
-	if (err) {
-		throw err;
-	}
+    if (err) {
+        throw err;
+    }
 });
 
 var User = require('./api/user/user.model');
@@ -18,77 +18,81 @@ Group.remove({}, function () {});
 Payment.remove({}, function () {});
 
 var alice = new User(
-		{
-			firstName: "Alice",
-			lastName: "Impsum",
-			email: "example@email.com",
-			phone: "+33000000000",
-			password: "azerty"
-		}
+        {
+            firstName: "Alice",
+            lastName: "Impsum",
+            email: "example@email.com",
+            phone: "+33000000000",
+            password: "azerty"
+        }
 );
 var aliceID = null;
 
 var bob = new User(
-		{
-			firstName: "Bob",
-			lastName: "Lorem",
-			email: "email@example.com",
-			phone: "+33000000001",
-			password: "012345"
-		}
+        {
+            firstName: "Bob",
+            lastName: "Lorem",
+            email: "email@example.com",
+            phone: "+33000000001",
+            password: "012345"
+        }
 );
 var bobID = null;
 
 alice.save(function (err, res) {
-	if (err)
-		throw err;
-	console.log(res);
-	aliceID = res["_id"];
-	bob.save(function (err, res) {
-		if (err)
-			throw err;
-		console.log(res);
-		bobID = res["_id"];
-		var aliceBob = new Group(
-				{
-					name: "Alice & Bob",
-					friends: [aliceID, bobID]
-				}
-		);
-		aliceBob.save(function (err, res)
-		{
-			if (err)
-				throw err;
-			console.log(res);
-			var aliceBobID = res["_id"];
-			var restaurant = new Bill(
-					{
-						name: "Restaurant",
-						description: "Un petit restaurant à deux payé par Alice",
-						owner: aliceID,
-						group: aliceBobID,
-						currency: "EUR",
-						value: 90
-					}
-			);
-			restaurant.save(function (err, res) {
-				console.log(res);
-			});
-			var hostel = new Bill(
-					{
-						name: "Auberge de jeunesse",
-						description: "Un nuit en auberge de jeunesse payée par Bob",
-						owner: bobID,
-						group: aliceBobID,
-						currency: "EUR",
-						value: 36
-					}
-			);
-			hostel.save(function (err, res) {
-				console.log(res);
-			});
-		});
-	});
+    if (err)
+        throw err;
+    console.log(res);
+    aliceID = res["_id"];
+    bob.save(function (err, res) {
+        if (err)
+            throw err;
+        console.log(res);
+        bobID = res["_id"];
+        var aliceBob = new Group(
+                {
+                    name: "Alice & Bob",
+                    friends: [aliceID, bobID]
+                }
+        );
+        aliceBob.save(function (err, res)
+        {
+            if (err)
+                throw err;
+            console.log(res);
+            var aliceBobID = res["_id"];
+            var restaurant = new Bill(
+                    {
+                        name: "Restaurant",
+                        description: "Un petit restaurant à deux payé par Alice",
+                        owner: aliceID,
+                        group: aliceBobID,
+                        currency: "EUR",
+                        value: 90
+                    }
+            );
+            restaurant.save(function (err, res) {
+                if (err)
+                    throw err;
+                console.log(res);
+                var hostel = new Bill(
+                        {
+                            name: "Auberge de jeunesse",
+                            description: "Un nuit en auberge de jeunesse payée par Bob",
+                            owner: bobID,
+                            group: aliceBobID,
+                            currency: "EUR",
+                            value: 36
+                        }
+                );
+                hostel.save(function (err, res) {
+                    if (err)
+                        throw err;
+                    console.log(res);
+                    console.log("Jeu de données ajouté.");
+                    process.exit();
+                });
+            });
+        });
+    });
 });
-
-console.log("Jeu de données ajouté.");
