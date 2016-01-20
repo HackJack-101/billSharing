05a6fdb25efca1f506573403229547d060a4a73c
SylEmo <sylvain.emonet@orange.fr>
2016-01-20 23:27:25 +0100

    Bon aller j'envoie tout là c'est parti go go go. On voit ça demain

--- client/js/app.js
+++ client/js/app.js
@@ -5,11 +5,15 @@ var app = angular.module('app', ['ngRoute', 'ngMaterial']);
 app.config(['$routeProvider',
     function ($routeProvider) {
         $routeProvider.when('/', {templateUrl: 'partials/home.html'});
-        $routeProvider.when('/recentactivity', {templateUrl: 'partials/recentactivity.html'});
-        $routeProvider.when('/allbills', {templateUrl: 'partials/userbills.html'});
         $routeProvider.when('/login', {templateUrl: 'partials/login.html'});
         $routeProvider.when('/register', {templateUrl: 'partials/register.html'});
+        $routeProvider.when('/recentactivity', {templateUrl: 'partials/recentactivity.html'});
+        $routeProvider.when('/allbills', {templateUrl: 'partials/userbills.html'});
+        $routeProvider.when('/allgroups', {templateUrl: 'partials/usergroups.html'});
+        $routeProvider.when('/activitygrp', {templateUrl: 'partials/activitygroup.html'});
+        $routeProvider.when('/allfriends', {templateUrl: 'partials/userfriends.html'});
         $routeProvider.when('/addgroup', {templateUrl: 'partials/addgroup.html'});
+        $routeProvider.when('/account', {templateUrl: 'partials/account.html'});
         $routeProvider.otherwise({
             redirectTo: "/"
         });
