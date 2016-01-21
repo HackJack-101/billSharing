ee685dcc661437a807e644532a0ba827988ae7ec
Jack3113 <jack03113@gmail.com>
2016-01-21 10:50:56 +0100

    App Controllers

--- client/js/app.js
+++ client/js/app.js
@@ -1,25 +1,56 @@
 'use strict';
 
 var app = angular.module('app', ['ngRoute', 'ngMaterial',
-                         'app.controller.user', 'app.service.user',
-                         'app.controller.bill', 'app.service.bill', 
-                         'app.controller.group', 'app.service.group', 
-                         'app.controller.payement', 'app.service.payement']);
+    'app.controller.user', 'app.service.user',
+    'app.controller.bill', 'app.service.bill',
+    'app.controller.group', 'app.service.group',
+    'app.controller.payement', 'app.service.payement']);
 
 app.config(['$routeProvider',
     function ($routeProvider) {
-        $routeProvider.when('/', {templateUrl: 'partials/home.html'});
-        $routeProvider.when('/login', {templateUrl: 'partials/login.html'});
-        $routeProvider.when('/register', {templateUrl: 'partials/register.html'});
-        $routeProvider.when('/recentactivity', {templateUrl: 'partials/recentactivity.html'});
-        $routeProvider.when('/allbills', {templateUrl: 'partials/userbills.html'});
-        $routeProvider.when('/allgroups', {templateUrl: 'partials/usergroups.html'});
-        $routeProvider.when('/activitygrp', {templateUrl: 'partials/activitygroup.html'});
-        $routeProvider.when('/allfriends', {templateUrl: 'partials/userfriends.html'});
-        $routeProvider.when('/addgroup', {templateUrl: 'partials/addgroup.html'});
-        $routeProvider.when('/account', {templateUrl: 'partials/account.html'});
-        $routeProvider.otherwise({
-            redirectTo: "/"
-        });
+        $routeProvider.
+                when('/', {
+                    templateUrl: 'partials/home.html',
+                    controller: ''
+                }).
+                when('/login', {
+                    templateUrl: 'partials/login.html',
+                    controller: ''
+                }).
+                when('/register', {
+                    templateUrl: 'partials/register.html',
+                    controller: ''
+                }).
+                when('/recentactivity', {
+                    templateUrl: 'partials/recentactivity.html',
+                    controller: ''
+                }).
+                when('/allbills', {
+                    templateUrl: 'partials/userbills.html',
+                    controller: ''
+                }).
+                when('/allgroups', {
+                    templateUrl: 'partials/usergroups.html',
+                    controller: ''
+                }).
+                when('/activitygrp', {
+                    templateUrl: 'partials/activitygroup.html',
+                    controller: ''
+                }).
+                when('/allfriends', {
+                    templateUrl: 'partials/userfriends.html',
+                    controller: ''
+                }).
+                when('/addgroup', {
+                    templateUrl: 'partials/addgroup.html',
+                    controller: ''
+                }).
+                when('/account', {
+                    templateUrl: 'partials/account.html',
+                    controller: ''
+                }).
+                otherwise({
+                    redirectTo: "/"
+                });
     }
 ]);
