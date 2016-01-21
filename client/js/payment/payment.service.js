51b526dede5e75be66092cfc4538d1f679f8579e
Jack3113 <jack03113@gmail.com>
2016-01-21 17:36:09 +0100

    Update routes

--- client/js/payment/payment.service.js
+++ client/js/payment/payment.service.js
@@ -3,22 +3,22 @@
 angular.module('app.service.payment', ['app'])
 
 
-    .factory('Payment', function($http) {
-        return {
-            get : function(id) {
-                return $http.get('/payment/' + id);
-            },
-            getAll : function() {
-                return $http.get('/payment/');
-            },           
-            add : function(payment) {
-                return $http.post('/payment/', payment);
-            },
-            edit : function(payment) {
-                return $http.put('/payment/', payment);
-            },
-            delete : function(id) {
-                return $http.delete('/payment/' + id);
+        .factory('Payment', function ($http) {
+            return {
+                get: function (id) {
+                    return $http.get('/api/payment/' + id);
+                },
+                getAll: function () {
+                    return $http.get('/api/payment/');
+                },
+                add: function (payment) {
+                    return $http.post('/api/payment/', payment);
+                },
+                edit: function (payment) {
+                    return $http.put('/api/payment/', payment);
+                },
+                delete: function (id) {
+                    return $http.delete('/api/payment/' + id);
+                }
             }
-        }
-    });
+        });
